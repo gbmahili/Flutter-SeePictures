44b0535aaa6d9232020a4905ecb687e9963251d6
Baraka Mahili <georgesmahili@gmail.com>
2019-05-16 18:45:04 -0400

    Added container and more padding and margin

--- lib/src/widgets/image_list.dart
+++ lib/src/widgets/image_list.dart
@@ -3,15 +3,28 @@ import '../models/image_model.dart';
 
 class ImageList extends StatelessWidget {
   final List<ImageModel> images;
+  // Images will be passed in here...just like this.props.images in ReactNative (RN)
   ImageList(this.images);
 
+  // Those images will be passed into the ListView and displayed...just like FlatList in RN
   @override
   Widget build(BuildContext context) {
     return ListView.builder(
       itemCount: images.length,
-      itemBuilder: (context, int index) {
-        return Text(images[index].url);
-      },
+      //itemBuilder is like rederItem in RN
+      // For every element in the images List (array), we show a widget (Component)
+      itemBuilder: (context, int index) => displayImage(images, index),
+    );
+  }
+
+  displayImage(images, index) {
+    return Container(
+      decoration: BoxDecoration(
+        border: Border.all(color: Colors.grey),
+      ),
+      margin: const EdgeInsets.all(20.0),
+      padding: const EdgeInsets.all(20.0),
+      child: Image.network(images[index].url),
     );
   }
 }
