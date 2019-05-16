f7b33454c24d3b3bc761f58d84390f890ebe7dfd
Baraka Mahili <georgesmahili@gmail.com>
2019-05-16 07:56:34 -0400

    Refactored StatelessWidget to a StatefullWidget

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -13,7 +13,7 @@ import 'package:flutter/material.dart';
 
 // Create a class that will be our custom widget (compoent for ReactNative)
 // It must extends the 'StatelessWideget' base class
-class App extends StatelessWidget {
+class App_OLD_WITHOUT_STATE_NOT_BEING_USED extends StatelessWidget {
   // Stateless is more like a functional component in React Native
   // Must define a build method that returns the widgets that 'this' widget will show
   Widget build(context) {
@@ -34,3 +34,43 @@ class App extends StatelessWidget {
   }
 }
 // This component must be imported whereever it will be used: import 'app.dart'..to use it: App()
+
+// Convert the App Widget to a StatefullWidget
+
+// 1. First, create a class with Statefullness which returns the State classs
+class App extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return AppState();
+  }
+}
+
+// 2. Create the state of the app (data)
+class AppState extends State<App> {
+  // Stateless is more like a functional component in React Native
+  // Must define a build method that returns the widgets that 'this' widget will show
+
+  // value to be incremeneted
+  int counter = 0;
+
+  Widget build(context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("Baraka's Images"),
+        ),
+        body: Center(child: Text('$counter')),
+        floatingActionButton: FloatingActionButton(
+          child: Icon(Icons.add),
+          onPressed: () {
+            //increment the counter and update state
+            setState(() {
+              counter += 1;
+            });
+          },
+          tooltip: 'Add new image',
+        ),
+      ),
+    );
+  }
+}
